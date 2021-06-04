02258ab00b4504e02ce3e824b3717d3945b7e6a9
Amarildo Tardivo <amarildotardivo@gmail.com>
2021-06-04 19:03:26 -0300

    Criada Descrição do Sistema

---
+++ Distribuidora de Cosméticos/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Calendar;
+import java.util.Date;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		String nome = "Dan Gusfield";
+        int eu = nome.charAt(3);
+        System.out.println(nome);
+        System.out.println(eu);
+
+	}
+
+}
---
+++ Distribuidora de Cosméticos/src/teste.java
@@ -0,0 +1,6 @@
+import java.util.Calendar;
+import java.util.Date;
+
+public class teste {
+	
+}
